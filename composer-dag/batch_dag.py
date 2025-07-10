5e9d0a0fd8fe42ddee0e24a5bdd59e663f902c8b
eshanmalyala <eshanmalyala@outlook.com>
2025-07-10 06:41:12 +0530

    Update batch_dag.py

--- composer-dag/batch_dag.py
+++ composer-dag/batch_dag.py
@@ -1,25 +1,26 @@
-from airflow import DAG
-from airflow.providers.google.cloud.operators.dataflow import DataflowStartFlexTemplateOperator
+from airflow import models
+from airflow.providers.google.cloud.operators.dataflow import DataflowTemplateOperator
 from datetime import datetime
 
-with DAG(
-    'batch_customer_masking',
-    start_date=datetime(2025, 1, 1),
-    schedule_interval='@daily',
-    catchup=False
+with models.DAG(
+    dag_id='trigger_dataflow_dag',
+    start_date=datetime(2021, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["dataflow"],
 ) as dag:
 
-    run_batch = DataflowStartFlexTemplateOperator(
-        task_id='launch_batch_masking',
-        project_id='gcp-agent-garden',
-        location='europe-west1',
-        body={
-            "launch_parameter": {
-                "jobName": "batch-mask-job",
-                "containerSpecGcsPath": "gs://getwellsoon-bucket/templates/streaming_template.json",
-                "parameters": {
-                    "config_path": "dataflow/config.yaml"
-                }
-            }
-        }
+    run_dataflow = DataflowTemplateOperator(
+        task_id="run_dataflow_template",
+        template="gs://dataflow-templates-us-central1/latest/GCS_Text_to_BigQuery",
+        location="us-central1",
+        parameters={
+            "inputFilePattern": "gs://getwellsoon-bucket-0/user.csv",
+            "JSONPath": "gs://getwellsoon-bucket-0/bq.json",
+            "outputTable": "gcp-agent-garden:getwellsoon_dataset.employee",
+            "bigQueryLoadingTemporaryDirectory": "gs://myworkspace-579raj/",
+            "javascriptTextTransformGcsPath": "gs://getwellsoon-bucket-0/udf.js",
+            "javascriptTextTransformFunctionName": "transform"
+        },
+        gcp_conn_id='google_cloud_default'
     )
