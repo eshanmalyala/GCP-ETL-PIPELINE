f0a61f06d71f171eb04b310acc61764768e9a922
eshanmalyala <eshanmalyala@outlook.com>
2025-07-10 06:32:09 +0530

    Update udf.js

--- dataflow-batch-job/udf.js
+++ dataflow-batch-job/udf.js
@@ -1,17 +1,20 @@
 function transform(line) {
     var values = line.split(',');
-
-    // Check if the line is the header by checking the first value
-    if (values[0].toLowerCase() === 'id') {
-        return null; // Ignore the header row
+    if (values[0] == "EmployeeID") {
+        return null; // Skip header row
     }
-
+    // create object mapping the values to the BigQuery schema 
+    // EmployeeID,FirstName,LastName,Department,Position,Salary,JoiningDate,Country
     var obj = new Object();
-    obj.id = values[0];
-    obj.name = values[1];
-    obj.email = values[2];
-    obj.age = values[3];
-    obj.city = values[4];
+    obj.EmployeeID = parseInt(values[0]);
+    obj.FirstName = values[1];
+    obj.LastName = values[2];
+    obj.Department = values[3];
+    obj.Position = values[4];
+    obj.Salary = parseFloat(values[5]);
+    obj.JoiningDate = new Date(values[6]);
+    obj.Country = values[7];
     var jsonString = JSON.stringify(obj);
+    // return the JSON string   
     return jsonString;
 }
